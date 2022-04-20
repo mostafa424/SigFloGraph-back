92d44df13d0bf0d8ffef2fb49996237797325786
Omar Safa <safa.omar72@gmail.com>
2022-04-20 22:51:37 +0200

    first commit

---
+++ src/test/java/au/ctrl/prjct/PrjctApplicationTests.java
@@ -0,0 +1,13 @@
+package au.ctrl.prjct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjctApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
